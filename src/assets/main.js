5cfbae85b56f7b54180570b38b42f92ad34e2ab1
nzerr <nicolas.zerr@gmail.com>
2016-12-18 16:16:24 +0100

    null check

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,16 +4,27 @@ $(function () {
         url: 'https://www.codeschool.com/users/Zantetsu.json',
         dataType: 'jsonp',
         success: function (response) {
-            var courses = response.courses.completed;
-            for (var i = 0; i < courses.length; i++) {
+            
+            if (
+                response != null && 
+                response != undefined &&
+                response.courses != undefined && 
+                response.courses != null &&
+                response.courses.completed != null &&
+                response.courses.completed != undefined
+            ) 
+            {
+                var courses = response.courses.completed;
 
-                var element = "<div class='course'><h3>"
-                + courses[i].title
-                 + "</h3><img src='" + courses[i].badge
-                 + "'>" + "<a href='" + courses[i].url + "' target='_blank' class='btn - btn-primary'>See Course</a>"
-                 + "</div>";
+                for (var i = 0; i < courses.length; i++) {
+                    var element = "<div class='course'><h3>"
+                    + courses[i].title
+                     + "</h3><img src='" + courses[i].badge
+                     + "'>" + "<a href='" + courses[i].url + "' target='_blank' class='btn - btn-primary'>See Course</a>"
+                     + "</div>";
 
-                $("#badges").append(element);
+                    $("#badges").append(element);
+                }
             }
         }
     });
