ac4a3c5997a9bef234874960619faf36dee441a7
nzerr <nicolas.zerr@gmail.com>
2016-12-18 12:27:40 +0100

    Added badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
-$(function() {
+$(function () {
 
-  // your code will go here
+    $.ajax({
+        url: 'https://www.codeschool.com/users/Zantetsu.json',
+        dataType: 'jsonp',
+        success: function (response) {
+            var courses = response.courses.completed;
+            for (var i = 0; i < courses.length; i++) {
+                $("#badges").append("<div class='course'><h3>" + courses[i].title
+                 + "</h3><img src='" + courses[i].badge 
+                 + "'>" + "<a href='" + courses[i].url + "' target='_blank' class='btn - btn-primary'>See Course</a>"
+                 + "</div>");
+            }
+        }
+
+    });
 
 });
